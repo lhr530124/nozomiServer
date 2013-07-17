b125612296427026117e29205e3eee0844c36d95
liyonghelpme <liyonghelpme@gmail.com>
2013-07-17 14:13:10 +0800

    merge redis code

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DEBUG = True
 PASSWORD = '2e4n5k2w2x'
 ADMINS = ['233242872@qq.com', 'caesars321@gmail.com', '21024851@qq.com']
 HOSTPORT = 9010
-SORTPORT = 9011
+SORTPORT = 9021
 
 CHATPORT = 8004
 CHATSERVER = 'http://localhost:%d/sys' % (CHATPORT)
