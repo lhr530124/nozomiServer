547ce9345794c5293d6c2f9449d5de197e330143
liyonghelpme <liyonghelpme@gmail.com>
2013-07-17 13:59:22 +0800

    syn mysql to redis

---
+++ script/synUserRankToRedis.py
@@ -0,0 +1,27 @@
+#coding:utf8
+import sys
+sys.path.append('..')
+import MySQLdb
+import config
+import redis
+
+def getConn():
+    return MySQLdb.connect(host=config.HOST, user='root', passwd=config.PASSWORD, db=config.DATABASE, charset='utf8')
+myCon = getConn()
+
+
+
+rserver = redis.Redis()
+
+myCon.query('flush table with read lock')
+sql = 'select * from nozomi_rank'
+myCon.query(sql)
+res = myCon.store_result().fetch_row(0, 1)
+for i in res:
+    rserver.zadd('userRank', i['uid'], i['score'])
+
+myCon.query('unlock table')
+
+myCon.close()
+    
+
