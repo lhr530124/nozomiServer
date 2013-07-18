02596f277df54f2cb815b3a917f20ddd6afed04c
liyonghelpme <liyonghelpme@gmail.com>
2013-07-18 09:31:42 +0800

    merge chinese

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 HOST = 'localhost'
 DATABASE = 'nozomi'
 DEBUG = True
-PASSWORD = '2e4n5k2w2x'
+PASSWORD = 'badperson3'
 ADMINS = ['233242872@qq.com', 'caesars321@gmail.com', '21024851@qq.com']
 HOSTPORT = 9012
 SORTPORT = 9003
