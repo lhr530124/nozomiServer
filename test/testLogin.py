c809a9b0cba958804b85137ac9e4fd891881412d
liyonghelpme <liyonghelpme@gmail.com>
2013-07-18 09:27:44 +0800

    testLogin

---
+++ test/testLogin.py
@@ -0,0 +1,34 @@
+#coding:utf8
+from testConfig import *
+r = base2+'login'
+data = {
+'username': 'TEST1'
+}
+req2(r, data)
+
+r = base2+'getData?uid=3'
+req(r)
+
+
+#同步200个建筑物信息
+#删除 
+#更新建筑物
+#
+
+r = base2+'synData'
+up = []
+for i in xrange(1, 200):
+    up.append({'buildIndex':i, 'grid':4000, 'bid':10, 'level':5, 'time':5, 'hitpoints':10})
+
+data = {
+'uid':1, 
+'delete':json.dumps([0, 1, 2, 3]), 
+'update':json.dumps(up), 
+'achieves':json.dumps([[10, 10, 0]]), 
+'research':json.dumps([1, 2, 2, 2, 2, 2, 2, 2, 2, 2]),
+'userInfo':json.dumps({'shieldTime':10, 'guideValue':1400}),
+'eid':1,
+'stat': 'finidLog',
+'crystal': json.dumps([1, 2, 3]),
+}
+req2(r, data)
