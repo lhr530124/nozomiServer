a7d341561630ef1245f63ae9bf6df7c9932a37d0
liyonghelpme <liyonghelpme@gmail.com>
2013-07-18 17:10:52 +0800

    尝试使用newrelic 来监控后台服务器

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DATABASE = 'nozomi'
 DEBUG = True
 PASSWORD = '2e4n5k2w2x'
 ADMINS = ['233242872@qq.com', 'caesars321@gmail.com', '21024851@qq.com']
-HOSTPORT = 9010
+HOSTPORT = 9020
 SORTPORT = 9021
 
 CHATPORT = 8004
