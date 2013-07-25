7353064c4506d66478021ed63c14f3b5b5b3f9e3
liyonghelpme <liyonghelpme@gmail.com>
2013-07-25 13:37:59 +0800

    MailHandler

---
+++ BufferMailHandler.py
@@ -0,0 +1,17 @@
+#coding:utf8
+import logging
+import logging.handlers
+import time
+class BufferMailHandler(logging.handlers.SMTPHandler):
+    def __init__(self, mailhost, fromaddr, toaddr, subject, credentials=None, secure=None):
+        logging.handlers.SMTPHandler.__init__(self, mailhost, fromaddr, toaddr, subject, credentials, secure)
+        self.lastSendTime = 0
+    def emit(self, record):
+        now = int(time.time())
+        if now - self.lastSendTime > 3600:
+            self.lastSendTime = now
+            logging.handlers.SMTPHandler.emit(self, record)
+        else:
+            #距离上次发送时间不够不发送
+            pass
+        
