43ecfeefe2bb56c1b2a1f2ad59dbf98479bc11f3
liyonghelpme <liyonghelpme@gmail.com>
2013-07-28 02:27:39 +0000

    modify config

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ DATABASE = 'nozomi'
 DEBUG = True
 PASSWORD = 'wavegame1'
 ADMINS = ['233242872@qq.com', 'caesars321@gmail.com', '21024851@qq.com']
-HOSTPORT = 9907
-SORTPORT = 9908
+HOSTPORT = 9917
+SORTPORT = 9918
 CHATPORT = 8005
 CHATSERVER = 'http://localhost:%d/sys' % (CHATPORT)
 
