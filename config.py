7bb81244b797f4e416072b49dd052822724f7715
liyonghelpme <liyonghelpme@gmail.com>
2013-07-28 07:56:43 +0000

    statistics charge number

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ DATABASE = 'nozomi'
 DEBUG = True
 PASSWORD = 'wavegame1'
 ADMINS = ['233242872@qq.com', 'caesars321@gmail.com', '21024851@qq.com']
-HOSTPORT = 9917
-SORTPORT = 9918
+HOSTPORT = 9927
+SORTPORT = 9928
 CHATPORT = 8005
 CHATSERVER = 'http://localhost:%d/sys' % (CHATPORT)
 
