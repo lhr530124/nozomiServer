8cea3585c97c20b9af70fb6ec067d37e89241cfe
liyonghelpme <liyonghelpme@gmail.com>
2013-08-01 09:18:50 +0800

    buyCrystal log

--- util.py
+++ util.py
@@ -3,3 +3,13 @@ beginTime = (2013, 1, 1, 0, 0, 0, 0, 0, 0)
 beginTime = int(time.mktime(beginTime))
 def getTime():
     return int(time.time()-beginTime)
+
+def getToday():
+    nt = time.localtime()
+    today = time.mktime((nt.tm_year, nt.tm_mon, nt.tm_mday, 0, 0, 0, 0, 0, 0 ))
+    return today-beginTime
+
+def getYesterday():
+    nt = time.localtime()
+    yesterday = time.mktime((nt.tm_year, nt.tm_mon, nt.tm_mday-1, 0, 0, 0, 0, 0, 0 ))
+    return yesterday-beginTime
