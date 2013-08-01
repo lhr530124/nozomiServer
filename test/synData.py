84129e93fd71c95d6a69e373c14cd046bf31c156
liyonghelpme <liyonghelpme@gmail.com>
2013-08-01 15:16:16 +0800

    可能在一次同步数据的过程中 有用户购买超过1次的水晶 则水晶价位 标注明晰出来

--- test/synData.py
+++ test/synData.py
@@ -2,16 +2,21 @@
 #coding:utf8
 from testConfig import *
 
-base2 = "http://23.21.135.42:9927/"
+#base2 = "http://23.21.135.42:9927/"
+base2 = 'http://localhost:%d/' % (config.HOSTPORT)
 r = base2+'synData'
-data = {
-'uid':2, 
-'delete':json.dumps([]), 
-'update':json.dumps([]), 
-'achieves':json.dumps([]), 
-'userInfo':json.dumps({'crystal':5000}),
-'research':json.dumps([1, 2, 2, 2, 2, 2, 2, 2, 2, 2]),
-'crystal':json.dumps([[1, 1989, 100]]),
-'eid':1,
-}
-req2(r, data)
+testValue = (0, 99, 0, 500, 0, 27000, 0, 21000, 0, 5500, 0,  5600)
+#testValue = (0, 99)
+for v in testValue:
+    data = {
+    'uid':2, 
+    'delete':json.dumps([]), 
+    'update':json.dumps([]), 
+    'achieves':json.dumps([]), 
+    'userInfo':json.dumps({'crystal':v}),
+    'research':json.dumps([1, 2, 2, 2, 2, 2, 2, 2, 2, 2]),
+    #'crystal':json.dumps([[1, 1989, 0]]),
+    'eid':1,
+    }
+    req2(r, data)
+
