c93d26c391c00b50944a1c1bb512d924efe08473
liyonghelpme <liyonghelpme@gmail.com>
2013-08-12 12:53:52 +0800

    在app 中增加mysql 请求时间的统计

--- app2.py
+++ app2.py
@@ -15,6 +15,8 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 from logging import Formatter
 
+from MySQLdb import cursors, connections
+
 """
 HOST = 'localhost'
 DATABASE = 'nozomi'
@@ -25,6 +27,35 @@ PASSWORD = '2e4n5k2w2x'
 
 reload(sys)
 sys.setdefaultencoding('utf-8') 
+
+#record mysql query time
+mysqlLogHandler = TimedRotatingFileHandler('mysqlSortLog.log', 'd', 1)
+
+mysqllogger = logging.getLogger("mysqlLogger")
+mysqllogger.addHandler(mysqlLogHandler)
+mysqllogger.setLevel(logging.INFO)
+
+#oldExec = getattr(cursors.BaseCursor, 'execute')
+oldQuery = getattr(connections.Connection, 'query')
+
+"""
+def execute(self, query, args=None):
+    startTime = time.time()*1000
+    oldExec(self, query, args)
+    endTime = time.time()*1000
+    mysqlLogHandler.info("%s %d", query, int(endTime-startTime))
+"""
+    
+def query(self, sql):
+    startTime = time.time()*1000
+    oldQuery(self, sql)
+    endTime = time.time()*1000
+    mysqllogger.info("%s\t%d", sql, int(endTime-startTime))
+
+#setattr(cursor.BaseCursor, 'execute', execute)
+setattr(connections.Connection, 'query', query)
+
+
 
 app = Flask(__name__)
 app.config.from_object("config")
