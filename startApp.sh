7323cec804856dfc9de7d3f936a40c86922cde2a
liyonghelpme <liyonghelpme@gmail.com>
2013-08-17 09:39:19 +0800

    cn start

--- startApp.sh
+++ startApp.sh
@@ -1,1 +1,1 @@
-NELIC_CONFIG_FILE=/root/NozomiEnv/newrelic.ini newrelic-admin run-program python app.py
+NELIC_CONFIG_FILE=/root/NozomiEnv/newrelic.ini newrelic-admin run-program gunicorn -w 4 -b 0.0.0.0:9060 app:app
