4f7fe1c02bc7bec9ffb23ca5d1984ed4e48a4160
liyonghelpme <liyonghelpme@gmail.com>
2013-08-22 13:02:35 +0800

    敏感词测试 和 使用方法说明

--- test/testFilter.py
+++ test/testFilter.py
@@ -1,6 +1,14 @@
+#coding:utf8
 from testConfig import *
 r = base2+'checkKeyWord?'+urllib.urlencode({'word':"fuck fuck you"})
 req(r)
 
+r = base2+'checkKeyWord?'+urllib.urlencode({'word':"中国人民大团结万岁"})
+req(r)
+
 r = base2+'blockWord?'+urllib.urlencode({'word':'fuck fuck you'})
 req(r)
+
+r = base2+'blockWord?'+urllib.urlencode({'word':"中国人民大团结万岁"})
+req(r)
+
