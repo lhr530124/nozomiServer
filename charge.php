2f064d1d589fbe1f5d4db9b0ccf2ec560d0413b9
liyonghelpme <liyonghelpme@gmail.com>
2013-08-27 15:15:07 +0800

    new charge code

--- charge.php
+++ charge.php
@@ -3,17 +3,25 @@
 <meta content="text/html; charset=UTF-8">
 </head>
 <body>
-<br> crystal 500
+<?php
+$item_name=$_GET["item_name"];
+echo "<br> $item_name";
+?>
 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method='post'>
 <input type="hidden" name="cmd" value="_xclick">
 <input type="hidden" name="business" value="liyonghelpme4@qq.com">
-<input type="hidden" name="item_name" value="crystal 500">
+
 <input type="hidden" name="item_number" value="1">
-<input type="hidden" name="amount" value="399.00">
-<input type="hidden" name="currency_code" value="USD">
+
 <?php
 $invoice=$_GET["invoice"];
+$item_name=$_GET["item_name"];
+$amount=$_GET["amount"];
+$currency_code=$_GET["currency_code"];
+echo "<input type='hidden' name='item_name' value='$item_name'>";
 echo "<input type='hidden' name='invoice' value='$invoice'>";
+echo "<input type='hidden' name='amount' value='$amount'>";
+echo "<input type='hidden' name='currency_code' value='$currency_code'>";
 ?>
 <input type="hidden" name="return" value="http://www.caesarsgame.com/success.php">
 <input type="hidden" name="cancel_return" value="http://www.caesarsgame.com/cancel.html">
