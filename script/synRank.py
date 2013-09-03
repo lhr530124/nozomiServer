7efb890fc6ea7f43a3e2c9ba0b93ae2283c95748
liyonghelpme <liyonghelpme@gmail.com>
2013-09-03 02:46:36 +0000

    rank syn

---
+++ script/synRank.py
@@ -0,0 +1,10 @@
+import MySQLdb
+con = MySQLdb.connect(host='localhost', passwd='wavegame1', db='nozomi', user='root', charset='utf8')
+sql = 'select id, score from nozomi_user'
+con.query(sql)
+res = con.store_result().fetch_row(0, 1)
+for i in res:
+    sql = 'update nozomi_user_state set score = %d where uid = %d' % (i['score'], i['id'])
+    con.query(sql)
+con.commit()
+con.close()
