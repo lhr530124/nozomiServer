381b246c73e11660281b7d4e0e477868709f76d2
root <root@ip-10-151-82-234.ec2.internal>
2013-09-04 01:41:08 +0000

    fix remove all localhost just use inner ip

--- app.py
+++ app.py
@@ -57,14 +57,6 @@ setattr(connections.Connection, 'query', query)
 
 
 
-"""
-HOST = 'localhost'
-DATABASE = 'nozomi'
-DEBUG = True
-PASSWORD = '2e4n5k2w2x'
-"""
-
-
 reload(sys)
 sys.setdefaultencoding('utf-8') 
 
