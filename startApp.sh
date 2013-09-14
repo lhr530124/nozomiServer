b86d13b46ff8bada6a633be4c28116708ee3f530
liyonghelpme <liyonghelpme@gmail.com>
2013-09-14 03:34:09 +0000

    updateScore syn app2 score

--- startApp.sh
+++ startApp.sh
@@ -1,1 +1,1 @@
-NELIC_CONFIG_FILE=/root/NozomiEnv/newrelic.ini newrelic-admin run-program gunicorn -w 1 -b 0.0.0.0:9800 app:app
+NELIC_CONFIG_FILE=/root/NozomiEnv/newrelic.ini newrelic-admin run-program gunicorn -w 1 -b 0.0.0.0:9830 app:app
