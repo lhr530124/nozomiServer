e4d9183d427ef4fed3d959d71909f94bd8dbfd23
liyong <liyonghelpme@gmail.com>
2013-10-02 06:59:08 +0000

    backup nozomi2

--- script/backup.sh
+++ script/backup.sh
@@ -2,3 +2,4 @@
 date_str=`date +%Y%m%d`
 cd /data/backup
 mysqldump -hlocalhost -uroot -pwavegame1 -R -E -e nozomi | gzip > nozomi_$date_str.sql.gz
+mysqldump -hlocalhost -uroot -pwavegame1 -R -E -e nozomi2 | gzip > nozomi2_$date_str.sql.gz
