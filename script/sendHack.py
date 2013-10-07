22771d3ffdb5e05672447f19d7887fedb01ec617
liyonghelpme <liyonghelpme@gmail.com>
2013-10-07 09:04:18 +0800

    hack news

---
+++ script/sendHack.py
@@ -0,0 +1,12 @@
+import urllib
+import urllib2
+data = {
+'uid':1,
+'name':'Caesars Studio',
+'cid':0,
+'text':'Hackers are forbidden! We will kick all hackers out when found.'
+}
+r = urllib.urlopen('http://localhost:8009/send?'+urllib.urlencode(data))
+l = r.read()
+print l
+
