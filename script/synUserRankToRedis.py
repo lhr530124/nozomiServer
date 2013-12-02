687ad7e60a2023913bdd80ea01a682ccfdb2cb64
liyonghelpme <liyonghelpme@example.com]>
2013-12-02 09:42:41 +0800

    synRank data

--- script/synUserRankToRedis.py
+++ script/synUserRankToRedis.py
@@ -6,21 +6,21 @@ import config
 import redis
 
 def getConn():
-    return MySQLdb.connect(host=config.HOST, user='root', passwd=config.PASSWORD, db=config.DATABASE, charset='utf8')
+    return MySQLdb.connect(host=config.HOST, user=config.USER, passwd=config.PASSWORD, db=config.DATABASE, charset='utf8')
 myCon = getConn()
 
 
 
 rserver = redis.Redis()
 
-myCon.query('flush tables with read lock')
+#myCon.query('flush tables with read lock')
 sql = 'select id, score from nozomi_user'
 myCon.query(sql)
 res = myCon.store_result().fetch_row(0, 1)
 for i in res:
     rserver.zadd('userRank', i['id'], i['score'])
 
-myCon.query('unlock table')
+#myCon.query('unlock table')
 
 myCon.close()
     
